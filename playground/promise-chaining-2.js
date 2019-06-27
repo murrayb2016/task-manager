25496be1a5f2e7276c45f981b707df9e85118878
Murray <bwmurray@ndc.nasa.gov>
2019-06-27 14:54:05 -0500

    async-await routing & playground

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -1,10 +1,23 @@
 require('../src/db/mongoose');
 const Task = require('../src/models/task');
 
-Task.findByIdAndDelete('5d14dff3659cc83d1824d30c').then((res)=>{
-    return Task.countDocuments({completed: false});
-}).then((count)=>{
+// Task.findByIdAndDelete('5d14dff3659cc83d1824d30c').then((res)=>{
+//     return Task.countDocuments({completed: false});
+// }).then((count)=>{
+//     console.log(count);
+// }).catch((e)=>{
+//     console.log(e);
+// }); 
+
+const DeleteTaskAndCount = async (id)=>{
+    const deleteTask = await Task.findByIdAndDelete(id);
+    const incompleteTaskCount = await Task.countDocuments({completed:false}); 
+
+    return incompleteTaskCount; 
+};
+
+DeleteTaskAndCount('5d14fcc36943122b5cc3c5d2').then((count)=>{
     console.log(count);
-}).catch((e)=>{
-    console.log(e);
-})
+}).catch((error)=>{
+    console.log(error);
+});
