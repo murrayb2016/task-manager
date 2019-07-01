a6bbd0fbe326aff6f3a4cee3d2ce2e1f7a647a84
Murray <bwmurray@ndc.nasa.gov>
2019-07-01 12:18:43 -0500

    Adding routing

--- src/index.js
+++ src/index.js
@@ -3,117 +3,32 @@ require('./db/mongoose')
 const User = require('./models/user');
 const Task = require('./models/task')
 
+const userRouter = require('./routers/user');
+const taskRouter = require('./routers/task');
+
 const app = express(); 
 const port = process.env.PORT || 3000; 
 
 app.use(express.json());
-
-app.post('/users', async (req,res)=>{
-    const user = new User(req.body);
-    
-    try{
-        await user.save();
-        res.status(201).send(user);
-    }
-    catch{
-        res.status(400).send();
-    }
-
-    // user.save().then(()=>{
-    //     res.status(201).send(user);
-    // }).catch((error)=>{
-    //     res.status(400).send(error);
-    // });
-});
-
-app.get('/users', async (req,res)=>{
-    try{
-        const users = await User.find({});
-        res.send(users);
-    }catch(e){
-        res.status(500).send();
-    }
-    // User.find({}).then((users)=>{
-    //     res.send(users);
-    // }).catch((error)=>{
-    //     res.status(500).send();
-    // });
-});
-
-app.get('/users/:id', async (req,res)=>{
-    const _id = req.params.id;
-    try{
-        const user = await User.findById(_id);
-
-        if(!user){
-            return res.status(404).send();
-        }
-        res.send(user);
-    }
-    catch(e){
-        res.status(500).send();
-    }
-    // User.findById(_id).then((user)=>{
-    //     if(!user){
-    //         return res.statusCode(404).send();
-    //     }
-    //     res.send(user);
-    // }).catch((error)=>{
-    //     res.status(500).send();
-    // });
-});
-
-app.post('/tasks', async (req,res)=>{
-    const task = new Task(req.body);
-
-    try{
-        const tasks = await task.save(); 
-        res.status(201).send(task);
-    }
-    catch(e){
-        res.status(400).send(error);
-    }
-    // task.save().then(()=>{
-    //     res.status(201).send(task);
-    // }).catch((error)=>{
-    //     res.status(400).send(error);
-    // });
-});
-
-app.get('/tasks', async (req,res)=>{
-    
-    try{
-        const tasks = await Task.find({});
-        res.send(tasks);
-    }catch(e){
-        res.status(500).send();
-    }
-
-    // Task.find({}).then((tasks)=>{
-    //     res.send(tasks);
-    // }).catch((error)=>{
-    //     res.status(500).send();
-    // });
-});
-
-app.get('/tasks/:id', async (req,res)=>{
-    const _id = req.params.id;
-    
-    try{
-        const task = await Task.findById(_id)
-        res.send(task);
-    }
-    catch(e){
-        res.status(500).send();
-    }
-
-    // Task.findById(_id).then((task)=>{
-    //     res.send(task);
-    // }).catch((error)=>{
-    //     res.status(500).send();
-    // });
-});
+app.use(userRouter);
+app.use(taskRouter);
 
 app.listen(port, ()=>{
     console.log('Server is started on port '+ port);
 });
+
+const bcrypt = require('bcryptjs'); 
+
+const myFunction = async ()=>{
+    const password = 'Red12345!';
+    const hashedPassword = await bcrypt.hash(password, 8);
+
+    console.log(password); 
+    console.log(hashedPassword);
+
+    const isMatch = await bcrypt.compare('Red12345!', hashedPassword);
+    console.log(isMatch);
+
+}
+
+myFunction()
